ab9719af9ca4d03bb85655b0f3f93133403d0f8d
ibrahima kaba <ikkaba92@hotmail.fr>
2022-04-21 18:50:42 +0200

    yelmp mvp

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,14 +8,14 @@ class RestaurantsController < ApplicationController
   end
 
   def new
-    @restaurant = Restaurant.new # needed to instantiate the form_for
+    @review = Review.new
+    @restaurant = Restaurant.new
   end
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
     @restaurant.save
 
-    # no need for app/views/restaurants/create.html.erb
     redirect_to restaurant_path(@restaurant)
   end
 
@@ -27,15 +27,15 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
     @restaurant.update(restaurant_params)
 
-    # no need for app/views/restaurants/update.html.erb
+
     redirect_to restaurant_path(@restaurant)
   end
-  
+
   def destroy
     @restaurant = Restaurant.find(params[:id])
     @restaurant.destroy
 
-    # no need for app/views/restaurants/destroy.html.erb
+
     redirect_to restaurants_path
   end
 
